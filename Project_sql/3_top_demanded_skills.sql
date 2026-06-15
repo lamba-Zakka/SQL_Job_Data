692c0760c8358a18cee641bafbde9f65a3ee52c6
lamba-Zakka <lambazakka576@gmail.com>
2026-06-15 13:07:07 +0100

    push example

---
+++ Project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,21 @@
+/* 
+Question: What are most in-demand skills for data analyst 
+- join job_postings to inner join table similar to query 2
+- identify the top 5 in-demand skills for data analyst.
+- focus on all job postings. 
+- why? Retrieves the top 5 skills with the highest demand in the job market,
+    provideng insight into the most valuable skills for job seekers. 
+*/
+
+
+select 
+    skills,
+    count(skills_job_dim.job_id) as demand_count
+from 
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY
+    skills
+LIMIT
+    5;
